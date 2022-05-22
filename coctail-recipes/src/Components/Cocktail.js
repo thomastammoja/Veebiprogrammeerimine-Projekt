beba86415547c76c0d54cde24b122e25520dfed4
Thomas Tammoja <thomas.tammoja@carrentalgateway.com>
2022-05-22 22:13:31 +0300

    Added ingredients showing by hovering

--- coctail-recipes/src/Components/Cocktail.js
+++ coctail-recipes/src/Components/Cocktail.js
@@ -18,16 +18,63 @@ function Cocktail({ recipe }) {
         <p>{recipe.recipename}</p>
       </div>
 
-      <img src={recipe.image} alt="" onClick={() => setOpenModal(true)} />
-
-      <div className="cocktail__rating">
-        <small>Hinnang:</small>
-        {Array(recipe.rating)
-          .fill()
-          .map(() => (
-            <StarRateIcon className="cocktail__ratingIcon" />
-          ))}
+      <div
+        className="cocktail__imageContainer"
+        onClick={() => setOpenModal(true)}
+      >
+        <img className="cocktail__image" src={recipe.image} alt="" />
+        <table className="cocktail__ingredients">
+          <thead>
+            <tr>
+              <th>koostisosa</th>
+              <th>kogus</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{recipe.ingredient1}</td>
+              <td>{recipe.measure1}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient2}</td>
+              <td>{recipe.measure2}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient3}</td>
+              <td>{recipe.measure3}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient4}</td>
+              <td>{recipe.measure4}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient5}</td>
+              <td>{recipe.measure5}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient6}</td>
+              <td>{recipe.measure6}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient7}</td>
+              <td>{recipe.measure7}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient8}</td>
+              <td>{recipe.measure8}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient9}</td>
+              <td>{recipe.measure9}</td>
+            </tr>
+            <tr>
+              <td>{recipe.ingredient10}</td>
+              <td>{recipe.measure10}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
+
       {openModal && (
         <CocktailModal
           key={recipe.id}
