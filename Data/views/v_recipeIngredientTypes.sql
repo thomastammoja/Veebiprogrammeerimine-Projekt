23403bc52ef90dcce2e65e33b5ec6e82dd53ae44
Thomas Tammoja <thomas.tammoja@carrentalgateway.com>
2022-05-23 20:56:25 +0300

    Changed aliases in view

--- Data/views/v_recipeIngredientTypes.sql
+++ Data/views/v_recipeIngredientTypes.sql
@@ -1,3 +1,4 @@
+DROP VIEW coctails.v_recipeIngredientTypes CASCADE;
 CREATE VIEW coctails.v_recipeIngredientTypes AS (
 	SELECT
 		rec.id AS recipeId
@@ -12,73 +13,73 @@ CREATE VIEW coctails.v_recipeIngredientTypes AS (
 				ingType1.id = 1 OR ingType2.id = 1 OR ingType3.id = 1 OR ingType4.id = 1 OR	ingType5.id = 1 OR
 				ingType6.id = 1 OR ingType7.id = 1 OR ingType8.id = 1 OR ingType9.id = 1 OR ingType10.id = 1
 			THEN 1
-			ELSE 0 END AS includesVodka
+			ELSE 0 END AS Viin
 		,CASE
 			WHEN 
 				ingType1.id = 2 OR ingType2.id = 2 OR ingType3.id = 2 OR ingType4.id = 2 OR	ingType5.id = 2 OR
 				ingType6.id = 2 OR ingType7.id = 2 OR ingType8.id = 2 OR ingType9.id = 2 OR ingType10.id = 2
 			THEN 1
-			ELSE 0 END AS includesLiquer
+			ELSE 0 END AS Liköör
 		,CASE
 			WHEN 
 				ingType1.id = 3 OR ingType2.id = 3 OR ingType3.id = 3 OR ingType4.id = 3 OR	ingType5.id = 3 OR
 				ingType6.id = 3 OR ingType7.id = 3 OR ingType8.id = 3 OR ingType9.id = 3 OR ingType10.id = 3
 			THEN 1
-			ELSE 0 END AS includesGin
+			ELSE 0 END AS Gin
 		,CASE
 			WHEN 
 				ingType1.id = 4 OR ingType2.id = 4 OR ingType3.id = 4 OR ingType4.id = 4 OR	ingType5.id = 4 OR
 				ingType6.id = 4 OR ingType7.id = 4 OR ingType8.id = 4 OR ingType9.id = 4 OR ingType10.id = 4 
 			THEN 1
-			ELSE 0 END AS includesRum
+			ELSE 0 END AS Rumm
 		,CASE
 			WHEN 
 				ingType1.id = 5 OR ingType2.id = 5 OR ingType3.id = 5 OR ingType4.id = 5 OR	ingType5.id = 5 OR
 				ingType6.id = 5 OR ingType7.id = 5 OR ingType8.id = 5 OR ingType9.id = 5 OR ingType10.id = 5 
 			THEN 1
-			ELSE 0 END AS includesVermut
+			ELSE 0 END AS Vermut
 		,CASE
 			WHEN 
 				ingType1.id = 6 OR ingType2.id = 6 OR ingType3.id = 6 OR ingType4.id = 6 OR	ingType5.id = 6 OR
 				ingType6.id = 6 OR ingType7.id = 6 OR ingType8.id = 6 OR ingType9.id = 6 OR ingType10.id = 6 
 			THEN 1
-			ELSE 0 END AS includesWhiskey
+			ELSE 0 END AS Viski
 		,CASE
 			WHEN 
 				ingType1.id = 7 OR ingType2.id = 7 OR ingType3.id = 7 OR ingType4.id = 7 OR	ingType5.id = 7 OR
 				ingType6.id = 7 OR ingType7.id = 7 OR ingType8.id = 7 OR ingType9.id = 7 OR ingType10.id = 7 
 			THEN 1
-			ELSE 0 END AS includesTequila
+			ELSE 0 END AS Tekiila
 		,CASE
 			WHEN 
 				ingType1.id = 8 OR ingType2.id = 8 OR ingType3.id = 8 OR ingType4.id = 8 OR	ingType5.id = 8 OR
 				ingType6.id = 8 OR ingType7.id = 8 OR ingType8.id = 8 OR ingType9.id = 8 OR ingType10.id = 8 
 			THEN 1
-			ELSE 0 END AS includesCognac
+			ELSE 0 END AS Konjak
 		,CASE
 			WHEN 
 				ingType1.id = 9 OR ingType2.id = 9 OR ingType3.id = 9 OR ingType4.id = 9 OR	ingType5.id = 9 OR
 				ingType6.id = 9 OR ingType7.id = 9 OR ingType8.id = 9 OR ingType9.id = 9 OR ingType10.id = 9 
 			THEN 1
-			ELSE 0 END AS includesBrandy
+			ELSE 0 END AS Brändi
 		,CASE
 			WHEN 
 				ingType1.id = 10 OR ingType2.id = 10 OR ingType3.id = 10 OR ingType4.id = 10 OR	ingType5.id = 10 OR
 				ingType6.id = 10 OR ingType7.id = 10 OR ingType8.id = 10 OR ingType9.id = 10 OR ingType10.id = 10 
 			THEN 1
-			ELSE 0 END AS includesChampagne
+			ELSE 0 END AS Šampanja
 		,CASE
 			WHEN 
 				ingType1.id = 11 OR ingType2.id = 11 OR ingType3.id = 11 OR ingType4.id = 11 OR	ingType5.id = 11 OR
 				ingType6.id = 11 OR ingType7.id = 11 OR ingType8.id = 11 OR ingType9.id = 11 OR ingType10.id = 11 
 			THEN 1
-			ELSE 0 END AS includesBeer
+			ELSE 0 END AS Õlu
 		,CASE
 			WHEN 
 				ingType1.id = 12 OR ingType2.id = 12 OR ingType3.id = 12 OR ingType4.id = 12 OR	ingType5.id = 12 OR
 				ingType6.id = 12 OR ingType7.id = 12 OR ingType8.id = 12 OR ingType9.id = 12 OR ingType10.id = 12 
 			THEN 1
-			ELSE 0 END AS includesAperative
+			ELSE 0 END AS Aperatiiv
 	FROM coctails.recipe AS rec
 
 	LEFT JOIN coctails.ingredient AS ing1 ON rec.ingredient1Id = ing1.id
