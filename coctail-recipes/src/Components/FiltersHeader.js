9433f4a97d28913d21f28fb628b075591cbda9cc
Thomas Tammoja <thomas.tammoja@carrentalgateway.com>
2022-05-24 22:05:36 +0300

    Filtering improvement

--- coctail-recipes/src/Components/FiltersHeader.js
+++ coctail-recipes/src/Components/FiltersHeader.js
@@ -28,7 +28,6 @@ function FiltersHeader() {
         <FiltersPanel
           open={filtersOpen}
           onClose={(e) => setFiltersOpen(false)}
-          setFilters={(e) => setFilters(e)}
         />
       </div>
     </div>
